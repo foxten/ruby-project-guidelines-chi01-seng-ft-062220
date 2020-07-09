101418607594e958cc0be0390fdf43d1f59e6b91
msguttman <msguttman@gmail.com>
2020-07-09 03:41:15 -0500

    more CLI methods

--- lib/ui/menus.rb
+++ lib/ui/menus.rb
@@ -1,16 +1,20 @@
-$prompt = TTY::Prompt.new
 
+$prompt = TTY::Prompt.new
 
 def get_new_username
     puts "What should I call you?"
     print "> "
     name = gets.chomp
     $user = User.create(name: name)
+    system "cls"
     puts "Welcome!"
+    sleep(2)
+    select_film_menu()
 end
 
 
 def welcome
+    system "cls"
     puts "Welcome to Studio Ghibli Character Builder!"
     response = $prompt.select("Are you new here?", "Yes", "No")
 
@@ -28,6 +32,7 @@ def welcome
                 elsif User.all.find { |user| user.name == response }
                         $user = user
                         puts "Hi #{$user.name}!"
+                        select_film_menu()
                 else
                     puts "hmm... can't seem to find you"
                     get_new_username()
@@ -38,21 +43,31 @@ end
 
 def get_people_by_user
     #should return an array of people belonging to current user
+
+
 end
 
 
 def get_types_by_films_of_user
     #should return a list of films associated with people belonging to current user
+
+
 end
 
 
 def get_people_by_user_film
     #select people associated with current user, and then with current film
+
 end
 
 
 def select_film_menu
-    response = $prompt.select("What should we do?", "Work on existing film", "Make new film", "Go back")
+    #Entry Menu after User selection
+
+    response = $prompt.select("What should we do?",
+        "Choose existing film",
+        "Make new film",
+        "Exit")
         
         case
             when response == "Work on existing film"
@@ -61,8 +76,8 @@ def select_film_menu
             when response == "Make new film"
                 make_film_menu()
 
-            when response == "Go back"
-                main_menu()
+            when response == "Exit"
+                #end? 
         end
 end
 
@@ -73,8 +88,7 @@ def film_menu(film)
     response = $prompt.select("What should we do with #{film.name}",
         "See Characters",
         "See Species",        
-        "Go Back"
-        )
+        "Go Back")
     case
         when response == "See Characters"
             film_character_menu()
@@ -89,7 +103,9 @@ end
 
 
 def film_character_menu
-    
+   #Selects for active user people, then by film
+   #Display character list
+   #Options:
 end
 
 
